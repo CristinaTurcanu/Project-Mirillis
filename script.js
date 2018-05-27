f95b5913d614bc08d30bae82dca8d07192d22e25
TurcanuCristina <turcanu.cristina.v@gmail.com>
2018-05-27 15:24:10 +0300

    Just a project i created

---
+++ script.js
@@ -0,0 +1,9 @@
+menu.onclick = function myfunc(){
+	var x = document.getElementById("Topnav");
+
+	if(x.className === "topnav"){
+		x.className += " responsive";
+	}else{
+		x.className = "topnav";
+	}
+}
